dbb50f70ceb55b8d82ab3a9b1ed4865c8e9a5bd1
Jadessu <junioradessu@gmail.com>
2026-02-15 21:42:37 -0600

    n

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,4 +1,4 @@
 export type Theme = 'default' | 'valentines';
 
 /** Change this value to switch the site theme. Set to 'default' to disable. */
-export const ACTIVE_THEME: Theme = 'valentines';
+export const ACTIVE_THEME: Theme = 'default';
